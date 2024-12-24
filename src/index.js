8e0734e87d80624c8665e149f4d51609be36c96a
LuisThiermann <luis.ljate@gmail.com>
2024-12-24 17:08:02 +0100

    Added !commands

--- src/index.js
+++ src/index.js
@@ -69,6 +69,30 @@ client.on("messageCreate", async (message) => {
         
         message.reply(`<@${userId}>, you now have **${getPlusOneData(userId).length}** +1s.`);
     }
+
+    if(message.content === "!wb") {
+        message.reply(`if you're interested in joining World Boss fame farm activities, you shall open a ticket in https://discord.com/channels/1248205717379354664/1274422719168909323. Requirements are the following: Ability to use a scout while fame farming. 100 spec on weapon / offhand from https://discord.com/channels/1248205717379354664/1248254004962525255 If playing DPS, higher spec might be required Vouch of WB members (not mandatory but appreciated) Willingness to rat (!rat for more info).`)
+    }
+
+    if(message.content === "!rewards") {
+        message.reply(`Guild rewards a various set of activities, like delivering Power Cores to hideout or killing enemies in Unhallowed Cloister. To check what we redeem, https://discord.com/channels/1248205717379354664/1300766799209431101. \nTo redeem rewards, you can ask any Officer.`)
+    }
+
+    if(message.content === "!info") {
+        message.reply(`https://discord.com/channels/1248205717379354664/1274422719168909323 = Apply for Worldboss member role / issue WB releted complains. \nhttps://discord.com/channels/1248205717379354664/1248254004962525255 = Why not builds for WB \nhttps://discord.com/channels/1248205717379354664/1319310140222079006 = DPS and other tutorials made by our members. Follow these to get GOOD at your weapon and learn your rotations for WB. \nhttps://discord.com/channels/1248205717379354664/1267166145618640957 = NAPs \n"How to redeem balance? 💸 " - Contact any officer that is online and request your Discord Balance.`)
+    }
+
+    if(message.content === "!complain") {
+        message.reply(`If you have something to say or to complain about, you can open a ticket here https://discord.com/channels/1248205717379354664/1281971928231444490 \nOnly Overseers will be able to read it, so don't worry if you have an issue with an officer or caller.`)
+    }
+
+    if(message.content === "!rat") {
+        message.reply(`Ratting in WB means coming with an approved #"Guccirats" set or a T4 Rat set from Rat Gear tab in hideout bank and defend the worldboss from enemies trying to kill our pve party by knocking them into mobs / invis bombing / diving with SS gear. Usually once enemies are spotted, someone pings in https://discord.com/channels/1248205717379354664/1270502535702118400 or https://discord.com/channels/1229028530990350366/1229028531019841615 \n Once you came to defend the party, you can type \"+1 [Link of the call message]\" here: https://discord.com/channels/1248205717379354664/1316458591699341344 to have your participation counted by the bot.`)
+    }
+
+    if(message.content === "!regears") {
+        message.reply(`PVP Activity regear: T7 gear T8 Weapon. OC break included. Consumables / mounts not included. Ask caller to be sure. WB Regears: https://discord.com/channels/1248205717379354664/1250066776335843389`)
+    }
 });
 
 client.once("ready", () => {
