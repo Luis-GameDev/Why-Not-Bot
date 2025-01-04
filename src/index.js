518baee5fbe0d40a48b4057ecaacaae8c61a9daa
Luis-Dev <luis.ljate@gmail.com>
2025-01-04 19:31:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ function getPlusOneData(discordId) {
 }
 
 client.on("messageCreate", async (message) => {
-
+// 1
     if (message.content === "--stats" && message.author.id === process.env.OWNER_USER_ID) {
         operateWeeklyStatsTrack()
     }
