420b8f2dbc060e3b6ff1b8304a62cfed8589f8c4
Luis-Dev <luis.ljate@gmail.com>
2025-01-24 17:18:01 +0100

    Refactoring +1s fetch and set code

---
+++ src/plusones.js
@@ -0,0 +1,22 @@
+function addRatPlus(userId, date) {
+
+}
+
+function addCtaPlus(userId, callerId) {
+
+}
+
+function getRatPlusAmount(userId) {
+
+}
+
+function getCtaPlusAmount(userId) {
+
+}
+
+module.exports = {
+    addRatPlus,
+    addCtaPlus,
+    getRatPlusAmount,
+    getCtaPlusAmount
+};
