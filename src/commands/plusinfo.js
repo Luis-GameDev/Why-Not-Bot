1fc9359e70c2cd6c7d2f111433e3b11cf682297a
Luis-Dev <luis.ljate@gmail.com>
2025-02-14 18:24:59 +0100

    Refactored /plusinfo command, now supports all +1 categories

--- src/commands/plusinfo.js
+++ src/commands/plusinfo.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
+const Plusones = require("../plusones.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -18,47 +19,85 @@ module.exports = {
                 .addChoices(
                     { name: 'rat', value: 'rat' },
                     { name: 'content', value: 'content' },
-                    { name: 'cta', value: 'cta' }
+                    { name: 'cta', value: 'cta' },
+                    { name: 'focus', value: 'focus' },
+                    { name: 'vod', value: 'vod' },
+                    { name: 'scout', value: 'scout' }
                 )
         ),
     async execute(interaction) {
-        if(interaction.options.getString('type') === 'rat') {
-            let dataFilePath = path.join(__dirname, '../data/plusones.json');
+        const type = interaction.options.getString('type');
+        const member = interaction.options.getUser('member');
+        const discordId = member.id;
+        let plusones;
+        let fieldInfo;
+        let date;
 
-            let member = interaction.options.getUser('member');
-            let discordId = member.id;
+        switch (type) {
+            case 'rat':
+            plusones = Plusones.getRatPlus(discordId);
+            break;
+            case 'cta':
+            plusones = Plusones.getCtaPlus(discordId);
+            break;
+            case 'content':
+            plusones = Plusones.getContentPlus(discordId);
+            break;
+            case 'focus':
+            plusones = Plusones.getFocusPlus(discordId);
+            break;
+            case 'vod':
+            plusones = Plusones.getVodPlus(discordId);
+            break;
+            case 'scout':
+            plusones = Plusones.getScoutPlus(discordId);
+            break;
+        }
 
-            if (!fs.existsSync(dataFilePath)) {
-                return interaction.reply({
-                    content: 'No data file found.',
-                    ephemeral: true
-                });
-            }
-
-            let data = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
-            let plusOnes = data[discordId] || [];
-
-            if (plusOnes.length === 0) {
-                return interaction.reply({
-                    content: `<@${discordId}> has no RAT +1s recorded.`,
-                    ephemeral: true
-                });
-            }
+        
 
             let embeds = [];
             let currentEmbed = new EmbedBuilder()
-                .setTitle(`${plusOnes.length} RAT +1s for ${member.username}`)
+                .setTitle(`${plusones.length} ${type} +1s for ${member.username}`)
                 .setColor(0x1e90ff)
                 .setTimestamp();
 
-            plusOnes.forEach((entry, index) => {
-                let date = entry.date;
-                let fieldValue = `#${index + 1}: ${date}`;
+            plusones.forEach((entry, index) => {
+            
+                switch (type) {
+                    case 'rat':
+                    fieldInfo = `${entry.date}`;
+                    break;
+                    case 'cta':
+                    date = new Date(entry.time);
+                    fieldInfo = `Caller: <@${entry.caller}> at ${date.toLocaleString()}`;
+                    if (entry.link) {
+                        fieldInfo += `: ${entry.link}`;
+                    }
+                    break;
+                    case 'content':
+                    date = new Date(entry.time);
+                    fieldInfo = `Caller: <@${entry.caller}> at ${date.toLocaleString()}`;
+                    break;
+                    case 'focus':
+                    date = new Date(entry.time);
+                    fieldInfo = `${date.toLocaleString()}`;
+                    break;
+                    case 'vod':
+                    date = new Date(entry.time);
+                    fieldInfo = `Reviewed by <@${entry.reviewer}> at ${date.toLocaleString()}`;
+                    break;
+                    case 'scout':
+                    date = new Date(entry.time);
+                    fieldInfo = `${entry.date} at ${date.toLocaleString()}`;
+                    break;
+                }
+                let fieldValue = `#${index + 1}: ${fieldInfo}`;
 
                 if (currentEmbed.data.fields?.length === 25) {
                     embeds.push(currentEmbed);
                     currentEmbed = new EmbedBuilder()
-                        .setTitle(`${plusOnes.length} RAT +1s for ${member.username}`)
+                        .setTitle(`${plusones.length} ${type} +1s for ${member.username}`)
                         .setColor(0x1e90ff)
                         .setTimestamp();
                 }
@@ -73,126 +112,9 @@ module.exports = {
             }
 
             return interaction.reply({
-                content: `Listing all RAT +1s for <@${discordId}>...`,
+                content: `Listing all ${type} +1s for <@${discordId}>...`,
                 ephemeral: true
             });
-        }
-
-        if(interaction.options.getString('type') === 'cta') {
-            let dataFilePath = path.join(__dirname, '../data/ctaplusones.json');
-
-            let member = interaction.options.getUser('member');
-            let discordId = member.id;
-
-            if (!fs.existsSync(dataFilePath)) {
-                return interaction.reply({
-                    content: 'No data file found.',
-                    ephemeral: true
-                });
-            }
-
-            let data = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
-            let plusOnes = data[discordId] || [];
-
-            if (plusOnes.length === 0) {
-                return interaction.reply({
-                    content: `<@${discordId}> has no CTA +1s recorded.`,
-                    ephemeral: true
-                });
-            }
-
-            let embeds = [];
-            let currentEmbed = new EmbedBuilder()
-                .setTitle(`${plusOnes.length} +1s for ${member.username}`)
-                .setColor(0x1e90ff)
-                .setTimestamp();
-
-            plusOnes.forEach((entry, index) => {
-                let time = new Date(entry.time).toLocaleString();
-                let caller = entry.caller;
-                let link = entry.link ? entry.link : ' ';
-
-                let fieldValue = `#${index + 1}: Caller - <@${caller}> at ${time}: ${link}`;
-
-                if (currentEmbed.data.fields?.length === 25) {
-                    embeds.push(currentEmbed);
-                    currentEmbed = new EmbedBuilder()
-                        .setTitle(`${plusOnes.length} CTA +1s for ${member.username}`)
-                        .setColor(0x1e90ff)
-                        .setTimestamp();
-                }
-
-                currentEmbed.addFields({ name: '\u200B', value: fieldValue });
-            });
-
-            embeds.push(currentEmbed);
-
-            for (let embed of embeds) {
-                await interaction.channel.send({ embeds: [embed] });
-            }
-
-            return interaction.reply({
-                content: `Listing all CTA +1s for <@${discordId}>...`,
-                ephemeral: true
-            });
-        }
-
-        if(interaction.options.getString('type') === 'content') {
-            let dataFilePath = path.join(__dirname, '../data/contentplusones.json');
-
-            let member = interaction.options.getUser('member');
-            let discordId = member.id;
-
-            if (!fs.existsSync(dataFilePath)) {
-                return interaction.reply({
-                    content: 'No data file found.',
-                    ephemeral: true
-                });
-            }
-
-            let data = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
-            let plusOnes = data[discordId] || [];
-
-            if (plusOnes.length === 0) {
-                return interaction.reply({
-                    content: `<@${discordId}> has no Content +1s recorded.`,
-                    ephemeral: true
-                });
-            }
-
-            let embeds = [];
-            let currentEmbed = new EmbedBuilder()
-                .setTitle(`${plusOnes.length} +1s for ${member.username}`)
-                .setColor(0x1e90ff)
-                .setTimestamp();
-
-            plusOnes.forEach((entry, index) => {
-                let time = new Date(entry.time).toLocaleString();
-                let caller = entry.caller;
-
-                let fieldValue = `#${index + 1}: Caller - <@${caller}> at ${time}`;
-
-                if (currentEmbed.data.fields?.length === 25) {
-                    embeds.push(currentEmbed);
-                    currentEmbed = new EmbedBuilder()
-                        .setTitle(`${plusOnes.length} Content +1s for ${member.username}`)
-                        .setColor(0x1e90ff)
-                        .setTimestamp();
-                }
-
-                currentEmbed.addFields({ name: '\u200B', value: fieldValue });
-            });
-
-            embeds.push(currentEmbed);
-
-            for (let embed of embeds) {
-                await interaction.channel.send({ embeds: [embed] });
-            }
-
-            return interaction.reply({
-                content: `Listing all Content +1s for <@${discordId}>...`,
-                ephemeral: true
-            });
-        }
+        
     }
 };
