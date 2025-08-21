750998b19b20f5564b1b0dbd596cd5725a0e0e48
Luis-Dev <luis.ljate@gmail.com>
2025-08-21 21:26:56 +0200

    added voice to info

--- src/commands/info.js
+++ src/commands/info.js
@@ -3,6 +3,12 @@ const Plusones = require("../plusones.js");
 const fs = require("fs");
 const path = require('path');
 
+function msToTime(ms) {
+    const totalSeconds = Math.floor(ms / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    return `${hours}h ${minutes}m`;
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,32 +19,41 @@ module.exports = {
                 .setDescription('The member you want to get information about.')
                 .setRequired(true)
         ),
-        async execute(interaction) {
-                
-            const member = interaction.options.getMember("member")
-            const avatar = member.user.displayAvatarURL({dynamic: true})
+    async execute(interaction) {
+        const member = interaction.options.getMember("member")
+        const avatar = member.user.displayAvatarURL({ dynamic: true })
 
-            const filePath = path.join(__dirname, '../data/users', `${member.id}.json`);
+        const filePath = path.join(__dirname, '../data/users', `${member.id}.json`);
 
-            let userData;
-            if (fs.existsSync(filePath)) {
-                userData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-            } else {
-                userData = {ign: undefined};
-            }
-            const userIgn = userData.ign === undefined ? "Not Linked" : `IGN - ${userData.ign}`
+        let userData;
+        if (fs.existsSync(filePath)) {
+            userData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        } else {
+            userData = { ign: undefined, voicetime: { total: 0, sessions: [] } };
+        }
 
-            interaction.reply({embeds: [
-                new EmbedBuilder()
+        const userIgn = userData.ign === undefined ? "Not Linked" : `IGN - ${userData.ign}`;
+
+        const totalVoice = userData.voicetime?.total || 0;
+        const thisMonthSessions = (userData.voicetime?.sessions || []).filter(s => {
+            const joinDate = new Date(s.join);
+            const now = new Date();
+            return joinDate.getMonth() === now.getMonth() && joinDate.getFullYear() === now.getFullYear();
+        });
+
+        const monthlyVoice = thisMonthSessions.reduce((acc, s) => acc + (s.leave - s.join), 0);
+
+        interaction.reply({ embeds: [
+            new EmbedBuilder()
                 .setTitle(`${member.user.username}\`s Infocard`)
                 .addFields([
                     {
                         name: `Account created at:`,
-                        value: `<t:${Math.round(member.user.createdTimestamp/1000)}>`,
+                        value: `<t:${Math.round(member.user.createdTimestamp / 1000)}>`,
                     },
                     {
                         name: `Joined server at:`,
-                        value: `<t:${Math.round(member.joinedTimestamp/1000)}>`,
+                        value: `<t:${Math.round(member.joinedTimestamp / 1000)}>`,
                         inline: true
                     },
                     {
@@ -61,24 +76,28 @@ module.exports = {
                         Prio: ${await Plusones.getUserPrio(member.id)}`,
                     },
                     {
+                        name: "Voice Activity:",
+                        value: `Past 30d: ${msToTime(monthlyVoice)}\nTotal: ${msToTime(totalVoice)}`,
+                    },
+                    {
                         name: "Link:",
                         value: `${userIgn}`
                     },
                     {
-                        name: `Roles:`, 
+                        name: `Roles:`,
                         value: member.roles.cache
                             .filter(role => role.name !== '@everyone')
                             .map(role => `<@&${role.id}>`)
-                            .join(" ") || "-", 
-                        inline: false 
+                            .join(" ") || "-",
+                        inline: false
                     },
                     {
                         name: "Avatar:",
                         value: " "
-                    }                  
+                    }
                 ])
                 .setImage(avatar)
                 .setColor(0)
-            ]})
-        }
-}
+        ] });
+    }
+};
