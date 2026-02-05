5f35d253e90a37e53b3c6359d5bb8eec861521b3
Luis-Dev <luis.ljate@gmail.com>
2026-02-05 15:36:53 +0100

    Make IQ test interactions persistent

---
+++ src/iqQuestions.js
@@ -0,0 +1,69 @@
+const questions = [
+  {
+    question: "Do you understand that this IQ-Test needs to be taken seriously and completed within 5 minutes?",
+    choices: ["Yes", "No"],
+    correct: 0,
+  },
+  {
+    question: "What is the opposite of North-West?",
+    choices: ["North-East", "South-East", "South-West"],
+    correct: 1,
+  },
+  {
+    question: "What is the capital of France?",
+    choices: ["Berlin", "Madrid", "Paris", "Rome"],
+    correct: 2,
+  },
+  {
+    question: "Where is the sun rising from on the northern hemisphere?",
+    choices: ["East", "West", "North", "South"],
+    correct: 0,
+  },
+  {
+    question: "Spell 'cat' backwards.",
+    choices: ["act", "tac", "cta", "atc"],
+    correct: 1,
+  },
+  {
+    question: "What is the first letter of the English alphabet?",
+    choices: ["A", "B", "C", "D"],
+    correct: 0,
+  },
+  {
+    question: "If all Bloops are Razzies and all Razzies are Lazzies, are all Bloops necessarily Lazzies?",
+    choices: ["Yes", "No", "Maybe", "I don't know"],
+    correct: 0,
+  },
+  {
+    question: "What is the next number in the sequence: 2, 4, 8, 16, ...?",
+    choices: ["18", "20", "32", "24"],
+    correct: 2,
+  },
+  {
+    question: "Which of the following does not belong: apple, strawberry, carrot, grape?",
+    choices: ["apple", "strawberry", "carrot", "grape"],
+    correct: 2,
+  },
+  {
+    question: "Which number fits in the gap? [2] [6] [ ] [54] [162]",
+    choices: ["9", "18", "4", "28"],
+    correct: 1,
+  },
+  {
+    question: "What is 12 divided by 3?",
+    choices: ["2", "3", "4", "6"],
+    correct: 2,
+  },
+  {
+    question: "Rearrange the letters of 'LISTEN' to form another word.",
+    choices: ["SILENT", "ENLIST", "TINSEL", "All of the above"],
+    correct: 3,
+  },
+  {
+    question: "If a train travels at 60 miles per hour, how far does it travel in 2 hours?",
+    choices: ["60 miles", "120 miles", "180 miles", "240 miles"],
+    correct: 1,
+  },
+];
+
+module.exports = questions;
